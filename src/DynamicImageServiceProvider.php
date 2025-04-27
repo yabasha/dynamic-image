aacb1df4b1927218a7d0e8ba8154b9cb51addb56
Bashar Ayyash <bayyash.c@neoleap.com.sa>
2025-04-27 23:29:19 +0300

    Remove global helper function from DynamicImageServiceProvider

--- src/DynamicImageServiceProvider.php
+++ src/DynamicImageServiceProvider.php
@@ -12,23 +12,7 @@ class DynamicImageServiceProvider extends ServiceProvider
             __DIR__.'/../config/dynamicimage.php' => config_path('dynamicimage.php'),
         ], 'config');
 
-        // Register global helper
-        if (!function_exists('dynamic_image')) {
-            function dynamic_image($mode = null)
-            {
-                $config = config('dynamicimage');
-                $helper = new DynamicImageHelper(
-                    $config['folders'] ?? [],
-                    $config['extensions'] ?? [],
-                    $config['default_image'] ?? null
-                );
-                $mode = $mode ?: ($config['mode'] ?? 'random');
-                if ($mode === 'timed') {
-                    return $helper->timedImage($config['interval_minutes'] ?? 10);
-                }
-                return $helper->randomImage();
-            }
-        }
+
     }
 
     public function register()
